c08b0b6f3a8f317e54687d5d27910cf18f760812
Aravindhan Ayyanathan <aravindhan.a@ericsson.com>
2021-08-10 16:53:27 +0530

    Test case modified to run on vertx context

--- src/test/java/com/aravind/ReactiveGreetingResourceTest.java
+++ src/test/java/com/aravind/ReactiveGreetingResourceTest.java
@@ -1,7 +1,11 @@
 package com.aravind;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.vertx.core.Vertx;
+
 import org.junit.jupiter.api.Test;
+
+import javax.inject.Inject;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
@@ -9,13 +13,19 @@ import static org.hamcrest.CoreMatchers.is;
 @QuarkusTest
 public class ReactiveGreetingResourceTest {
 
+    @Inject
+    Vertx vertx;
+
     @Test
     public void testHelloEndpoint() {
-        given()
-          .when().get("/hello")
-          .then()
-             .statusCode(200)
-             .body(is("Hello RESTEasy Reactive"));
+        //The below test case should be a failure
+        vertx.runOnContext(unused -> {
+            given()
+                    .when().get("/hello")
+                    .then()
+                    .statusCode(500) //This is 200
+                    .body(is("Hello RESTEasy Reactive"));
+        });
     }
 
 }
